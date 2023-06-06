3494ba3d949941e8b94636306c881a1a32c91c26
Maryam <maryamjaved.xo@gmail.com>
2023-06-07 00:49:25 +0500

    trending

--- js/script.js
+++ js/script.js
@@ -32,27 +32,27 @@ const trendingDrinks = [
     "Mugicha",
 ]
 
-const cards = [
-    {
-        imgSrc: sushi12,
-        alt: "sushi-12",
-        title: "Chezu Sushi",
-        rating: "4.8",
-        price: "$21.00"
-    },
-    {
-        imgSrc: sushi11,
-        alt: "sushi-11",
-        title: "Originale Sushi",
-        rating: "4.8",
-        price: "$21.00",
-        active: true
-    },
-    {
-        imgSrc: sushi10,
-        alt: "sushi-10",
-        title: "Ramen Legendo",
-        rating: "4.8",
-        price: "$21.00"
-    }
-];
+// const cards = [
+//     {
+//         imgSrc: sushi12,
+//         alt: "sushi-12",
+//         title: "Chezu Sushi",
+//         rating: "4.8",
+//         price: "$21.00"
+//     },
+//     {
+//         imgSrc: sushi11,
+//         alt: "sushi-11",
+//         title: "Originale Sushi",
+//         rating: "4.8",
+//         price: "$21.00",
+//         active: true
+//     },
+//     {
+//         imgSrc: sushi10,
+//         alt: "sushi-10",
+//         title: "Ramen Legendo",
+//         rating: "4.8",
+//         price: "$21.00"
+//     }
+// ];
